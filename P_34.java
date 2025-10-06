dde142ecb06d2834795b67423dc870deb2a9a9e0
Shivam Sikarwar <163384691+shihvivamsikarwar@users.noreply.github.com>
2025-10-06 21:08:54 +0530

    aa

---
+++ P_34.java
@@ -0,0 +1,15 @@
+public class P_34 {
+    public static void main(String[] args){
+
+    }
+    static int[] searchRange(int[] nums, int target) {
+        
+    }
+    static int search(int[] nums, int target){
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            
+        }
+    }
+}
