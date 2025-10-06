037514b1e93fbba4fcfde13dbabefd4763a86ddc
Shivam Sikarwar <163384691+shihvivamsikarwar@users.noreply.github.com>
2025-10-06 19:01:47 +0530

    Day 1

---
+++ Problem_35.java
@@ -0,0 +1,24 @@
+public class Problem_35 {
+    public static void main(String[] args) {
+        int[] nums={1,3,5,6};
+        int target=7;
+        int ans=search(nums,target);
+        System.out.println(ans);
+        
+    }
+    static int search(int[] nums, int target) {
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(target<nums[mid]){
+                end=mid-1;
+            }else if(target>nums[mid]){
+                start=mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return start;
+    }
+}
