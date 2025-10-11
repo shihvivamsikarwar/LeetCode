23508548f23301838cdbd7d88c4af1618a23c7e4
Shivam Sikarwar <163384691+shihvivamsikarwar@users.noreply.github.com>
2025-10-11 20:37:21 +0530

    Find MaxConsecutiveOnes

---
+++ LeetCode_485.java
@@ -0,0 +1,20 @@
+public class LeetCode_485 {
+    public static void main(String[] args) {
+        int[] nums={1,0,1,1,0,1};
+        System.out.println(findMaxConsecutiveOnes(nums));
+    }
+    static int findMaxConsecutiveOnes(int[] nums) {
+        int maxCount=0;
+        int currentCount=0;
+        for(int num:nums){
+            if(num==1){
+                currentCount++;
+                maxCount=Math.max(maxCount, currentCount);
+            }else{
+                currentCount=0;
+            }
+        }
+        return maxCount;
+    }
+}
+
